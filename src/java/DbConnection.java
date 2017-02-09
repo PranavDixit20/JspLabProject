a65105d3d2a640c298512ea97b4961032bdc4b08
Pranav Dixit <pranavdixit20@gmail.com>
2017-02-09 18:14:40 +0530

    new commit

--- src/java/DbConnection.java
+++ src/java/DbConnection.java
@@ -9,29 +9,44 @@ public class DbConnection extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
        
-          response.setContentType("text/html;charset=UTF-8");
-          PrintWriter out = response.getWriter();
-          int i;
         
-          String Username=request.getParameter("uname");
-          String Password=request.getParameter("pswd");
+          response.setContentType("text/html;charset=UTF-8");
+         // PrintWriter out = response.getWriter();
+          int i;
           
-              if(LoginClass.validate(Username,Password)){
-                  if("admin".equals(Username) && "admin".equals(Password)){
-                      RequestDispatcher r=request.getRequestDispatcher("welcome.jsp");
-            r.forward(request, response);
+         ObjectOutputStream oos =new ObjectOutputStream(response.getOutputStream());
+        
+          String Username=request.getParameter("name");
+          String Password=request.getParameter("pass");
+          
+              if(validate(Username,Password)){
+                  //if("admin".equals(Username) && "admin".equals(Password)){
+                      oos.writeObject("success");
                   }
                   else{
-                      HttpSession hp = request.getSession();
-                      hp.setAttribute("nm", Username);
-                      RequestDispatcher r2=request.getRequestDispatcher("uwelcome.jsp");
-            r2.include(request, response);
+                      oos.writeObject("fail");
                   }
-           }
+          // }
           
-          else{
-            out.print("Fill user and pass");
+          
+          
+    }
+    public static boolean validate(String name,String pass){
+         boolean st=false;
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/library","root","8600525761");
+            PreparedStatement ps=con.prepareStatement("select * from login_table where user_id=? and password=?");
+            ps.setString(1,name);
+            ps.setString(2, pass);
+            ResultSet rs=ps.executeQuery();
+            st=rs.next();
         }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return st;
     }
 
  }
